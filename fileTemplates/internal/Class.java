1e001bc28dad2727f672c1a89cd9be084b590a23
hjwu <hjwu@travelsky.com>
2021-01-23 13:29:58 +0800

    IC-2020.3.1 <92826@DESKTOP-HI2TV6C Create debugger.xml, lombok-plugin.xml, colors.scheme.xml, updates.xml, setterTemplates.xml, editor.xml, projectView.xml, Default.xml, ide.general.xml, material_theme.xml, project.default.xml, baseRefactoring.xml, My Group.xml, mavenRunHelper.xml, wsl.distributions.xml, pluginAdvertiser.xml, markdown.xml, path.macros.xml, toStringTemplates.xml, laf.xml, ui.lnf.xml, usageView.xml, customization.xml, ide-features-trainer.xml, XML Properties File.xml, Default.xml, One Dark.icls, getterTemplates.xml, Class.java, jdk.table.xml, proxy.settings.xml, Eclipse copy.xml, notifications.xml, filetypes.xml, xml.xml, vcs.xml, mybatis.xml, templates.xml, find.xml, diff.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+*   ProjectName:
+*   ClassName: ${name}
+*   Class Description:  
+*   @Author hjwu
+*   @Date${DATE} ${TIME}
+*/
+public class ${NAME} {
+}
